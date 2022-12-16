a75ee2718d51893d7fbf32f9a410cb1aed9aab37
luizpadilhadev <luizfelipepadilha86@gmail.com>
2022-12-16 19:29:22 -0300

    primeira classe

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import boardgame.Position;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position pos = new Position (3, 5);
+        System.out.print(pos);
     }
 }
