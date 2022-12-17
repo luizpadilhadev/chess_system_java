3c6894aeb171507f7165f17b6965144fb0857566
luizpadilhadev <luizfelipepadilha86@gmail.com>
2022-12-17 10:50:31 -0300

    Peças em movimento

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -4,10 +4,32 @@ import Application.UI;
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+
+        while (true){
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performaceChessMove(source, target);
+        }
+
+
+
     }
 }
