db55882d6a8dd541d74239463304808c060729f7
luizpadilhadev <luizfelipepadilha86@gmail.com>
2022-12-18 15:10:16 -0300

    logica de checkmate

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,27 +1,24 @@
 package Application;
 
-import Application.UI;
-import boardgame.Board;
-import boardgame.Position;
-import chess.ChessException;
-import chess.ChessMatch;
-import chess.ChessPiece;
-import chess.ChessPosition;
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
+import chess.ChessException;
+import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -51,6 +48,8 @@ public class Main {
 				sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
 
